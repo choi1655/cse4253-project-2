ffc5c4545054e0ae032edd10a592c59546a8ee3d
johnchoi96 <johnchoi1003@icloud.com>
2020-10-07 02:45:50 -0400

    SceneGraph project added

---
+++ ISceneGraph/SceneGraph/GroupNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Choi.SceneGraph;
+
+namespace Choi.SceneGraphCore
+{
+    public class GroupNode: IGroupNode
+    {
+        public string Name
+        {
+            get;
+            private set;
+        }
+
+        public GroupNode()
+        {
+        }
+
+        public void Accept(IVisitor visitor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddChild(ISceneNode child)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Member variables
+        // List to keep track of the cihldren for the group
+        private IList<ISceneNode> children = new List<ISceneNode>(8);
+
+        #endregion
+    }
+}
---
+++ ISceneGraph/SceneGraph/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Choi.SceneGraph;
+
+namespace Choi.SceneGraphCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ISceneNode root;
+        }
+    }
+}
